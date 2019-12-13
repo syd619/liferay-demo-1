35aac8724d8c760ec6f9f1e28b93ef6c2b12240f
panagiotis raditsas <raditsas.panagiotis@gmail.com>
2019-12-13 18:17:12 +0200

    Initial commit

---
+++ src/lib/config/index.js
@@ -0,0 +1,4 @@
+export const SERVER = 'https://liferay72.open.gr';
+export const SITE_KEY = 'cosmoray';
+export const SITE_ID = '34243';
+export const AUTHORIZATION = 'Basic bGlmZXJheUBvcGVuLmdyOmxyNzIkb3BlbkBncg==';
